1ec613930948e8e27669eaa99c7137e0d54a5496
Abel_fan <842615663@qq.com>
2019-08-27 14:24:27 +0800

    add

--- python02/03流程控制.py
+++ python02/03流程控制.py
@@ -30,3 +30,50 @@ else:
 
 
 # 2、循环语句
+# for
+# 语法
+"""
+for 变量 in 可迭代对象:
+    循环体
+    
+for(var i=0;i<10;i++){
+    
+}
+"""
+
+# for item in "abcdef":
+#     print(item)
+
+# 遍历：依次访问列表中的元素
+# for item in [1,2,3,4,5,6]:
+#     print(item)
+# for item in (1,2,3,4,5,6):
+#     print(item)
+
+# range(start,end,step)  #
+
+# for i in range(10):
+#     print(i)
+
+
+# 嵌套for循环
+
+for i in range(10):
+    for j in range(10):
+        print("*")
+
+"""
+*****
+*****
+*****
+*****
+
+*
+**
+***
+****
+*****
+九九乘法表
+"""
+
+# while
