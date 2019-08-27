9c04db57399643785764f3f830bf46e16a52b81a
Abel_fan <842615663@qq.com>
2019-08-27 11:49:24 +0800

    分支语句

---
+++ python02/03流程控制.py
@@ -0,0 +1,32 @@
+# 流程控制
+# 1、分支语句
+# if
+# 语法
+"""
+#单路分支
+if a>2:
+    print(a)
+
+# 双路分支
+if a>b:
+    print("a>b")
+else:
+    print("b>a")
+
+# 多路分支
+if cj>90:
+    print("优秀")
+elif cj>85:
+    print("良好")
+elif cj > 60:
+    print("及格")
+else:
+    print("不及格")
+"""
+# 登录功能
+# username
+# password
+
+
+
+# 2、循环语句
